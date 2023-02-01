65dd1c402a7a03de401ca785290b30850b969790
mvill <119754846+migavg@users.noreply.github.com>
2023-01-31 17:32:21 -0700

    added more divs to the html to add more time slots along with appropriate local storage values for time in javascript

--- Develop/script.js
+++ Develop/script.js
@@ -4,9 +4,7 @@
 
 $(function () {
   var myDate = dayjs();
-  // var storageInput = document.querySelector(".description");
-
-
+  
 
   $(".saveBtn").on("click", function() {
     var description = $(this).siblings(".description").val();
@@ -19,6 +17,17 @@ $(function () {
 
 
 $("#hour-9 .description").val(localStorage.getItem("hour-9"));
+$("#hour-10 .description").val(localStorage.getItem("hour-10"));
+$("#hour-11 .description").val(localStorage.getItem("hour-11"));
+$("#hour-12 .description").val(localStorage.getItem("hour-12"));
+$("#hour-13 .description").val(localStorage.getItem("hour-13"));
+$("#hour-14 .description").val(localStorage.getItem("hour-14"));
+$("#hour-15 .description").val(localStorage.getItem("hour-15"));
+$("#hour-16 .description").val(localStorage.getItem("hour-17"));
+$("#hour-17 .description").val(localStorage.getItem("hour-17"));
+
+
+
 
 
 
